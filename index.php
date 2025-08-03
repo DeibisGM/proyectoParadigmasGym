864674bde29d4b57ae2706ec0390c2caf650316f
DeibisGM <jhongutierrezmorales517@gmail.com>
2025-08-03 13:42:58 -0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+// Este archivo será la única puerta de entrada a tu aplicación.
+// Desde aquí, incluyes los demás archivos que necesites.
+
+echo "Mi proyecto está funcionando!";
+
+// O, para cargar la vista principal de tu proyecto:
+// require_once 'view/loginView.php'; // O el archivo que sea tu página de inicio.
+
+?>
