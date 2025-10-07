3eddb473e711f783e7503ce9e7d056b416ee263b
Ciany-23 <cianyamadorsolis23@gmail.com>
2025-10-06 22:15:34 -0600

    listo

--- data/data.php
+++ data/data.php
@@ -32,21 +32,21 @@ class Data
                 $this->db = "dbgym";
                 break;
 
-            case "ciany-Inspiron-15-3515":
+            /*case "ciany-Inspiron-15-3515":
                 $this->isActive = false;
                 $this->server = "127.0.0.1";
                 $this->user = "ciany";
                 $this->password = "1223";
                 $this->db = "dbgym";
                 break;
-
+*/
             default:
                  $this->isActive = false;
-                                $this->server = "shortline.proxy.rlwy.net";
+                                $this->server = "mainline.proxy.rlwy.net";
                                 $this->user = "root";
-                                $this->password = "GobisQryHUevnFguFcxHzoFjRMBoxAuv";
+                                $this->password = "qZGxyvjECfahHipFJrnmbZUfjYakmNSq";
                                 $this->db = "railway";
-                                $this->port = 53962;
+                                $this->port = 20786;
 
                 break;
         }
